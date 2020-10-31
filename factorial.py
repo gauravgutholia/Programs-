0dd9d5724e1357846962b947aae1535d51d3e6a5
hackergaurav2 <73710372+hackergaurav2@users.noreply.github.com>
2020-10-31 13:33:28 -0400

    Python Program for Beginners

---
+++ factorial.py
@@ -0,0 +1,6 @@
+num = int(input("Enter the number: "))
+t = 1
+for i in range(1,num+1):
+     t=t*i
+     
+print(t)
---
+++ hacker2.py
@@ -0,0 +1,6 @@
+a=int(input())
+b=int(input())
+c=int((a/b))
+print(c)
+d=(a/b)
+print(d)
